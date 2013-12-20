328ec3493b59383f014aeeb92b4d26a15069f91c
Esko Luontola <esko.luontola@gmail.com>
2013-12-21 00:30:17 +0200

    Release scripts

---
+++ scripts/package.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+: ${1:? Usage: $0 VERSION}
+VERSION="$1"
+FILENAME="BuildPower-$VERSION.zip"
+set -x
+
+rm -rf dist/BuildPower
+mkdir -p dist
+cp -r src dist/BuildPower
+
+cd dist
+rm -f "$FILENAME"
+zip -r "$FILENAME" BuildPower
+rm -rf BuildPower
