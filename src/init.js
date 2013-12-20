3aa676c687c642914d0381db5fb6ffa3c72f8c91
Esko Luontola <esko.luontola@gmail.com>
2013-12-20 23:11:34 +0200

    Refactored

--- src/init.js
+++ src/init.js
@@ -1,22 +1,25 @@
 $(function () {
 
+    function hookFunction(obj, fnName, hookFn) {
+        var realFn = obj[fnName];
+        obj[fnName] = function () {
+            realFn.apply(obj, arguments);
+            hookFn.apply(obj, arguments);
+        };
+    }
+
     function loadTemplate(element, url) {
         element.load(url, function () {
             ko.applyBindings(model, element.get(0));
         });
     }
 
-    function beforeAdvice(obj, fnName, aspectFn) {
-        var realFn = obj[fnName];
-        obj[fnName] = function () {
-            aspectFn.apply(obj, arguments);
-            realFn.apply(obj, arguments);
-        };
-    }
+
+    // Hook this mod into PA
 
     model.buildPower = buildPower;
 
-    beforeAdvice(handlers, 'hover', buildPower.unitSpecs.update);
+    hookFunction(handlers, 'hover', buildPower.unitSpecs.update);
 
     $('.div_current_selection_cont').prepend('<div id="build_power"></div>');
     loadTemplate($('#build_power'), '../../mods/BuildPower/live_game/build_power.html');
