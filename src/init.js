461396003d7e0d888d1914a383b3068817fe7f42
Esko Luontola <esko.luontola@gmail.com>
2013-12-20 23:08:46 +0200

    Hooked in the unit specs updater

--- src/init.js
+++ src/init.js
@@ -6,7 +6,17 @@ $(function () {
         });
     }
 
+    function beforeAdvice(obj, fnName, aspectFn) {
+        var realFn = obj[fnName];
+        obj[fnName] = function () {
+            aspectFn.apply(obj, arguments);
+            realFn.apply(obj, arguments);
+        };
+    }
+
     model.buildPower = buildPower;
+
+    beforeAdvice(handlers, 'hover', buildPower.unitSpecs.update);
 
     $('.div_current_selection_cont').prepend('<div id="build_power"></div>');
     loadTemplate($('#build_power'), '../../mods/BuildPower/live_game/build_power.html');
