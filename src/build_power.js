0f0269ac081d4c21c9fdc19a6f51ad806fe25783
Esko Luontola <esko.luontola@gmail.com>
2013-12-20 11:59:18 +0200

    Refactored

--- src/build_power.js
+++ src/build_power.js
@@ -1,9 +1,18 @@
 $(function () {
+
+    function loadTemplate(element, url) {
+        element.load(url, function () {
+            ko.applyBindings(model, element.get(0));
+        });
+    }
+
     var fabricationRate = ko.observable(30); // TODO: calculate real value
     var energyConsumption = ko.observable(1500); // TODO: calculate real value
     var efficiency = ko.computed(function () {
         return fabricationRate() / energyConsumption() * 1000.0;
     });
+
+    // TODO: observe the min/max values of each unit type's consumption (and production), in order to calculate their fabrication rate and energy consumption
 
     model.buildPower = {
         metal: fabricationRate,
@@ -12,8 +21,5 @@ $(function () {
     };
 
     $('.div_current_selection_cont').prepend('<div id="build_power"></div>');
-    var injection = $('#build_power');
-    injection.load('../../mods/BuildPower/build_power.html', function () {
-        ko.applyBindings(model, injection.get(0));
-    });
+    loadTemplate($('#build_power'), '../../mods/BuildPower/build_power.html');
 });
